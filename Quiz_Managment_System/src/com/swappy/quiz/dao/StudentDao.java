ca9a0e5337cbe9999bcad3456759dbaa09998aa2
Swapnil Shinde <swapnilshinde9825@gmail.com>
2025-12-18 12:29:30 +0530

    Initial Logins

---
+++ Quiz_Managment_System/src/com/swappy/quiz/dao/StudentDao.java
@@ -0,0 +1,65 @@
+package com.swappy.quiz.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.swappy.quiz.entity.Users;
+import com.swappy.quiz.util.DbUtil;
+
+public class StudentDao implements AutoCloseable{
+	Connection con;
+	public Users currentuser = null;
+	public StudentDao() throws SQLException {
+		con=DbUtil.getConnection();
+	}
+
+	public boolean studentLogin(String email,String password) throws SQLException {
+		String sql = "select * from users where email=? and password_hash=?";
+		try(PreparedStatement stmt  = con.prepareStatement(sql)){
+			stmt.setString(1, email);
+			stmt.setString(2, password);
+			ResultSet rs = stmt.executeQuery();
+			
+			
+			if(rs.next()) {
+				currentuser= new Users();
+				currentuser.setId(rs.getInt(1));
+				currentuser.setName(rs.getString(2));
+				currentuser.setEmail(email);
+				currentuser.setPassword(password);
+				currentuser.setRole(rs.getString(5));
+				
+				return true;
+			}
+			return false;
+		}}
+		
+		public boolean studentAdd(Users user) throws SQLException {
+			
+			if(studentLogin(user.getEmail(), user.getPassword())) return false;
+			else {
+				
+			
+			String sql = "insert into users (name,email,password_hash) values(?,?,?)";
+			try(PreparedStatement stmt  = con.prepareStatement(sql)){
+				stmt.setString(1, user.getName());
+				stmt.setString(2, user.getEmail());
+				stmt.setString(3, user.getPassword());
+				 stmt.executeUpdate();
+				
+			}
+			return true;
+			}
+		
+	}
+	
+	@Override
+	public void close() throws SQLException {
+		if(con!=null) {
+			con.close();
+			con=null;
+		}
+	}
+}
--- Quiz_Managment_System/src/com/swappy/quiz/main/Main.java
+++ Quiz_Managment_System/src/com/swappy/quiz/main/Main.java
@@ -1,16 +1,15 @@
-package com.sunbeam.main;
+package com.swappy.quiz.main;
+
 import java.util.Scanner;
 
-import com.sunbeam.menu.MainMenu;
+import com.swappy.quiz.menu.MainMenu;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		MainMenu.mainMenu(sc);
 		sc.close();
-
 	}
 
 }
